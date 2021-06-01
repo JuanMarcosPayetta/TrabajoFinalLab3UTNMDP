84b7f718fe8fd8cbe5ee466cb6b634b2749594dc
doloresbruzzone <dolobruzzone@gmail.com>
2021-05-31 22:43:42 -0300

    clase Herramienta y sublcases
    atributos, constructores, setters, getters y toString

---
+++ TrabajoPracticoFinal/src/model/HerramientaJardineria.java
@@ -0,0 +1,48 @@
+package model;
+
+public abstract class HerramientaJardineria extends Producto {
+
+	private String material;
+	private String funcion; //riego, corte, limpieza, labrado, carga,...
+	
+	
+	public HerramientaJardineria() {
+		super();
+		this.material = null;
+		this.funcion = null;
+	}
+	public HerramientaJardineria(String nombre, String marca, double precio, int stock, String descripcion, String material, String funcion) {
+		super(nombre, marca, precio, stock, descripcion);
+		this.material = material;
+		this.funcion = funcion;
+	}
+	public HerramientaJardineria(String codigo, String nombre, String marca, String clasificacion, double precio,
+			int stock, String descripcion, String material, String funcion) {
+		super(codigo, nombre, marca, clasificacion, precio, stock, descripcion);
+		this.material = material;
+		this.funcion = funcion;
+	}
+	
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public String getFuncion() {
+		return funcion;
+	}
+	public void setFuncion(String funcion) {
+		this.funcion = funcion;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return super.toString() + ", material:" + material + ", funcion:" + funcion;
+	}
+	
+	
+	
+	
+}
---
+++ TrabajoPracticoFinal/src/model/HerramientaNoManual.java
@@ -0,0 +1,65 @@
+package model;
+
+public class HerramientaNoManual extends HerramientaJardineria {
+	
+	private int potencia;
+	private String tipoMotor; //eléctrico, combustible
+	private String consumo; //alto, medio, bajo
+	
+	public HerramientaNoManual() {
+		super();
+		this.potencia = 0;
+		this.tipoMotor = null;
+		this.consumo = null;
+	}
+	
+	public HerramientaNoManual(String nombre, String marca, double precio, int stock, String descripcion,
+			String material, String funcion, int potencia, String tipoMotor, String consumo) {
+		super(nombre, marca, precio, stock, descripcion, material, funcion);
+		this.potencia = potencia;
+		this.tipoMotor = tipoMotor;
+		this.consumo = consumo;
+	}
+	
+	public HerramientaNoManual(String codigo, String nombre, String marca, String clasificacion, double precio,
+			int stock, String descripcion, String material, String funcion, int potencia, String tipoMotor, String consumo) {
+		super(codigo, nombre, marca, clasificacion, precio, stock, descripcion, material, funcion);
+		this.potencia = potencia;
+		this.tipoMotor = tipoMotor;
+		this.consumo = consumo;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public String getTipoMotor() {
+		return tipoMotor;
+	}
+
+	public void setTipoMotor(String tipoMotor) {
+		this.tipoMotor = tipoMotor;
+	}
+
+	public String getConsumo() {
+		return consumo;
+	}
+
+	public void setConsumo(String consumo) {
+		this.consumo = consumo;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", potencia:" + potencia + ", tipoMotor:" + tipoMotor + ", consumo:" + consumo;
+	}
+
+	
+	
+	
+	
+}
