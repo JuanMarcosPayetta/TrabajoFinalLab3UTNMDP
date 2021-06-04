e87dae3783a135c28680479511c05d1b69bb4b2f
JuanMarcosPayetta <juanpayetta@gmail.com>
2021-06-04 18:09:47 -0300

    Actualizacion

--- TrabajoPracticoFinal/src/model/HerramientaNoManual.java
+++ TrabajoPracticoFinal/src/model/HerramientaNoManual.java
@@ -77,14 +77,63 @@ public class HerramientaNoManual extends HerramientaJardineria {
 		}
 	}
 	
+	/*
+	 * valida el consumo (bajo, medio, alto)
+	 */
+	public static String validarConsumoLlamada(String consumo)
+	{
+		String mensaje=null;
+		try {
+			validarConsumo(consumo);
+		} catch (InputMismatchException e) {
+			mensaje=e.getMessage();
+		} catch (NullPointerException e) {
+			mensaje=e.getMessage();
+		} catch (CadenaInvalidaException e) {
+			mensaje=e.getMessage();
+		}
+		
+		return mensaje;
+	}
 	
+	private static void validarConsumo(String consumo) throws NullPointerException, CadenaInvalidaException, InputMismatchException
+	{
+		ArrayList<String>consumoValido= new ArrayList<String>();
+		consumoValido.add("alto");
+		consumoValido.add("medio");
+		consumoValido.add("bajo");
+	
+		
+		if(consumo==null)
+		{
+			throw new NullPointerException("Error, ingrese un dato valido (alto, medio, bajo)");
+		}
+		else if(consumo.isBlank())
+		{
+			throw new CadenaInvalidaException(CadenaInvalidaException.ESPACIOENBLANCOEXCEPTION);
+		}
+		else if(!consumo.matches("[a-zA-Z]*\\D{4}"))
+		{
+			throw new CadenaInvalidaException(CadenaInvalidaException.LONGITUDYNUMEROSEXCEPTION+" 4 letras");
+		}
+		else if(!consumoValido.contains(consumo.toLowerCase()))
+		{
+			throw new InputMismatchException("Error, ingrese un dato valido (alto, medio, bajo)");
+		}
+	}
 	
 	public int getPotencia() {
 		return potencia;
 	}
 
-	public void setPotencia(int potencia) {
-		this.potencia = potencia;
+	public String setPotencia(int potencia) {
+		String mensaje=null;
+		mensaje=validarValorNumericoLlamada(potencia);
+		if(mensaje==null)
+		{
+			this.potencia = potencia;
+		}
+		return mensaje;
 	}
 
 	public String getTipoMotor() {
@@ -105,13 +154,20 @@ public class HerramientaNoManual extends HerramientaJardineria {
 		return consumo;
 	}
 
-	public void setConsumo(String consumo) {
-		this.consumo = consumo;
+	public String setConsumo(String consumo) {
+		String mensaje=null;
+		mensaje=validarConsumoLlamada(consumo);
+		if(mensaje==null)
+		{
+			this.consumo = consumo;
+		}
+		return mensaje;
 	}
 
+	
 	@Override
 	public String toString() {
-		return super.toString() + ", potencia:" + potencia + ", tipoMotor:" + tipoMotor + ", consumo:" + consumo;
+		return super.toString() + ", potencia: " + potencia + ", tipoMotor: " + tipoMotor + ", consumo: " + consumo;
 	}
 
 	
