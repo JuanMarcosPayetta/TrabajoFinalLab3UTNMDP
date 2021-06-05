db888baf30e073eb8ccc1461ce04a824628e9331
JuanMarcosPayetta <juanpayetta@gmail.com>
2021-06-04 22:22:04 -0300

    Actualizacion vivero

--- TrabajoPracticoFinal/src/domain/Vivero.java
+++ TrabajoPracticoFinal/src/domain/Vivero.java
@@ -1,13 +1,38 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import productos.Producto;
 
 public class Vivero {
 
-	//private HashMap<String, Producto>catalogoProductos;
-	//private HashMap<String, Servicio>catalogoServicios;
+	private HashMap<String, ArrayList<Producto>>catalogoProductos;
+	private HashMap<String, Servicio>catalogoServicios;
 	
 	
-	//public boolean obtenerCodigos
+	public ArrayList<String> obtenerCodigos()
+	{
+		ArrayList<Producto>productos=null;
+		ArrayList<String> codigos= new ArrayList<String>();
+		String codigoEncontrado=null;
+		Iterator<Map.Entry<String, ArrayList<Producto>>> it= catalogoProductos.entrySet().iterator();
+		while(it.hasNext())
+		{
+			Map.Entry<String, ArrayList<Producto>> entry= (Map.Entry<String, ArrayList<Producto>>)it.next();
+			productos=entry.getValue();
+			
+			for(int i=0; i<productos.size(); i++)
+			{
+				codigoEncontrado=productos.get(i).getCodigo();
+				codigos.add(codigoEncontrado);
+			}
+		}
+		
+		return codigos;	
+	}
 	
 }
+ 
---
+++ TrabajoPracticoFinal/src/interfaces/IVivero.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public interface IVivero{
+
+	public <T> void agregarElemento(T elemento); //para mapa y el metodo "existe" pregunto si T instanceof producto, y llamo al metodo en un if
+	public <T> String buscarElemento(T elemento);
+	public <T> String eliminarElemento(T elemento);
+	public <T> String mostrarElemento (T elemento);
+	//public String contar(int opcion); --> a debatir
+	//public String mostrarProductosYServicios(); // -->a debatir
+	
+	
+}
