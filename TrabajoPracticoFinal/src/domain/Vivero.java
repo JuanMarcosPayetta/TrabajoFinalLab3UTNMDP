abdbdf598b3f68c3f6cd1a92480370641a7c74cf
pablopayetta <pablopayetta@gmail.com>
2021-06-04 22:35:10 -0300

    Update Vivero.java

--- TrabajoPracticoFinal/src/domain/Vivero.java
+++ TrabajoPracticoFinal/src/domain/Vivero.java
@@ -8,39 +8,33 @@ import java.util.Map;
 import productos.Producto;
 
 public class Vivero {
-	
-	
+
 	/*
 	 * EL MAPA DE PRODUCTOS COMO CLAVE TIENE LA CLASIFICACION
 	 * ("ARBOL, ARBUSTO, ETC") Y DENTRO COMO VALOR TIENE UN ARRAY LIST CON TODOS LOS
 	 * ARBOLES, ARBUSTOS, ETC
 	 */
 
+	private HashMap<String, ArrayList<Producto>> catalogoProductos;
+	private HashMap<String, Servicio> catalogoServicios;
 
-	private HashMap<String, ArrayList<Producto>>catalogoProductos;
-	private HashMap<String, Servicio>catalogoServicios;
-	
-	
-	public ArrayList<String> obtenerCodigos()
-	{
-		ArrayList<Producto>productos=null;
-		ArrayList<String> codigos= new ArrayList<String>();
-		String codigoEncontrado=null;
-		Iterator<Map.Entry<String, ArrayList<Producto>>> it= catalogoProductos.entrySet().iterator();
-		while(it.hasNext())
-		{
-			Map.Entry<String, ArrayList<Producto>> entry= (Map.Entry<String, ArrayList<Producto>>)it.next();
-			productos=entry.getValue();
-			
-			for(int i=0; i<productos.size(); i++)
-			{
-				codigoEncontrado=productos.get(i).getCodigo();
-				codigos.add(codigoEncontrado);
+	public ArrayList<String> obtenerCodigos() {
+		ArrayList<Producto> productos = null;
+		ArrayList<String> codigos = new ArrayList<String>();
+		String codigoEncontrado = null;
+		Iterator<Map.Entry<String, ArrayList<Producto>>> it = catalogoProductos.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, ArrayList<Producto>> entry = (Map.Entry<String, ArrayList<Producto>>) it.next();
+			productos = entry.getValue(); // entry me devuelve tanto la clave como el valor, obtengo el arreglo en ese
+											// nivel
+
+			for (int i = 0; i < productos.size(); i++) { // recorro el arreglo obtenido y obtengo los codigos
+				codigoEncontrado = productos.get(i).getCodigo();
+				codigos.add(codigoEncontrado); // y guardo los codigos en un arreglo
 			}
 		}
-		
-		return codigos;	
+
+		return codigos;
 	}
-	
+
 }
- 
