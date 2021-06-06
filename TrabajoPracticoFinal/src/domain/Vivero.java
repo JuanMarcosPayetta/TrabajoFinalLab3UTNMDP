9e5e62635ec477ed38f76a07638db7537c7f44a2
pablopayetta <pablopayetta@gmail.com>
2021-06-06 15:01:24 -0300

    Update Vivero.java

--- TrabajoPracticoFinal/src/domain/Vivero.java
+++ TrabajoPracticoFinal/src/domain/Vivero.java
@@ -251,15 +251,17 @@ public class Vivero implements IVivero {
 			Map.Entry<String,ArrayList<Producto>> entrada=(Map.Entry<String, ArrayList<Producto>>)it.next();
 			ArrayList<Producto>arreglo=entrada.getValue();
 			
+			mensaje.append(entrada.getKey());
 			for(int i=0;i<arreglo.size();i++)
 			{
-				mensaje.append("[" + arreglo.get(i).toString() + "]\n");
+				mensaje.append(" [" + arreglo.get(i).toString() + " ] \n");
 			}
-
 		}
-
 		return mensaje.toString();
 	}
+	
+	
+	
 	
 	
 
