ca08eeb78d3824b6ef07dc98e81535a2de187096
pablopayetta <pablopayetta@gmail.com>
2021-06-06 20:43:06 -0300

    modicar producto
    actualizacion

--- TrabajoPracticoFinal/src/domain/Vivero.java
+++ TrabajoPracticoFinal/src/domain/Vivero.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
+
 import interfaces.IVivero;
 import productos.Producto;
 
@@ -322,7 +323,44 @@ public class Vivero implements IVivero {
 		return mensaje;
 	}
 	
+	public String modificarStockAumenta(Producto elProducto,int stock)
+	{
+		String mensaje="Producto no Ecnontrado";
+		
+		int flag=0;
+		Iterator<Map.Entry<String,ArrayList<Producto>>>it=catalogoProductos.entrySet().iterator();
+		
+		while(it.hasNext()&&flag==0)
+		{
+			Map.Entry<String,ArrayList<Producto>>entrada=(Map.Entry<String, ArrayList<Producto>>)it.next();
+			if(entrada.getKey().equalsIgnoreCase(elProducto.getClasificacion()))
+			{
+				ArrayList<Producto>arreglo=entrada.getValue();
+				for(int i=0;flag==0 && i<arreglo.size();i++)
+				{
+					if(arreglo.get(i).getCodigo().equals(elProducto.getCodigo()))
+					{
+						mensaje=arreglo.get(i).aumentarStock(stock);
+						if(mensaje==null)//si la exepcion  no devuelve error
+						{
+							catalogoProductos.replace(elProducto.getClasificacion(), arreglo);
+							flag=1;
+							mensaje="Nuevo Stock Establecido";
+						}
+					}
+				}
+			}
+		}
+		return mensaje;
+	}
 	
+	public String modificarStockDisminuye(Producto elProducto,int stock)
+	{
+		String mensaje="Producto no encontrado";
+		
+		
+		return mensaje;
+	}
 	
 	
 	
