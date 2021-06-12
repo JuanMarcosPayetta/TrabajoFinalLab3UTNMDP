c8e312b08c1ff348e106b8ffb6e293fc2e27f181
JuanMarcosPayetta <juanpayetta@gmail.com>
2021-06-12 00:05:30 -0300

    Switch

--- TrabajoPracticoFinal/src/app/Main.java
+++ TrabajoPracticoFinal/src/app/Main.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 import domain.AccesoDatos;
 import domain.Empleado;
 import domain.Vivero;
+import productos.Planta;
+import productos.PlantaAcuatica;
+import productos.Producto;
 
 public class Main {
 
@@ -18,8 +21,6 @@ public class Main {
 		Vivero vivero= new Vivero();
 		AccesoDatos acceso= new AccesoDatos();
 		
-		
-
 		acceso.leerArchivoProductos(vivero);
 		acceso.leerArchivoServicios(vivero);
 		acceso.leerArchivoEmpleados(vivero);
@@ -119,6 +120,7 @@ public class Main {
 			if(validarEmpleado)
 			{
 				char opcion2=0;
+				int opcion3=0;
 		
 				//while(opcion2!='n')
 				{
@@ -127,9 +129,232 @@ public class Main {
 					System.out.println("2-Ingresar un servicio al sistema: \n");
 					System.out.println("3-Ingresar un cliente al sistema: \n");
 			
+					opcion3=scan.nextInt();
+					scan.nextLine();
+					
 					switch (opcion) {
 					case 1: {
+						System.out.println("Seleccione el producto a ingresar: \n");
+						System.out.println("1-Planta Acuatica: \n");
+						System.out.println("2-Arbol: \n");
+						System.out.println("3-Arbusto: \n");
+						System.out.println("4-Hierba: \n");
+						System.out.println("5-Sustrato: \n");
+						System.out.println("6-Semillas: \n");
+						System.out.println("7-Sanidad Vegetal: \n");
+						System.out.println("8-Lombrices: \n");
+						System.out.println("9-Maceta Redonda: \n");
+						System.out.println("10-Maceta Poliedro: \n");
+						System.out.println("11-Herramienta manual: \n");
+						System.out.println("12-Herramienta electrica/combustible: \n");
+						System.out.println("13-Mueble de jardin: \n");
+						System.out.println("14-Decoracion: \n");
 						
+						opcion=scan.nextInt();
+						scan.nextLine();
+						
+						String nombre=null;
+						String marca=null;
+						int stock=0;
+						String descripcion=null;
+						int mesesDeVida=0;
+						String estacionPlantacion=null;
+						String habitat=null;
+						int altura=0;
+						boolean tieneflor=false;
+						String nivelDeExposicionSolar=null;
+						String tipoRaiz=null;
+						boolean esAromatica=false;
+						String tipoDeAgua=null;
+						int temperaturaAgua=0;
+						String durezaAgua=null;
+						String tipo=null;
+						
+						switch (opcion) {
+						case 1: {
+
+							mensaje="";
+							while(mensaje!=null)
+							{
+								System.out.println("Ingrese el nombre por favor");
+								nombre=scan.nextLine();
+							    mensaje=Producto.validarCadenaCaracteresLlamada(nombre);
+							    if(mensaje!=null)
+							    {
+							    	System.out.println(mensaje);
+							    }
+							} 
+							
+							mensaje="";
+							while(mensaje!=null)
+							{
+								System.out.println("Ingrese la marca por favor");
+								marca=scan.nextLine();
+							    mensaje=Producto.validarCadenaCaracteresLlamada(marca);
+							    if(mensaje!=null)
+							    {
+							    	System.out.println(mensaje);
+							    }
+							} 
+							
+							mensaje="";
+							while(mensaje!=null)
+							{
+								System.out.println("Ingrese el stock actual por favor");
+								stock=scan.nextInt();
+								scan.nextLine();
+							    mensaje=Producto.validarValorNumericoLlamada(stock);
+							    if(mensaje!=null)
+							    {
+							    	System.out.println(mensaje);
+							    }
+							} 
+							
+								System.out.println("Ingrese la marca por favor");
+								descripcion=scan.nextLine();
+							   
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese los meses de vida actual de la planta por favor");
+									mesesDeVida=scan.nextInt();
+									scan.nextLine();
+								    mensaje=Producto.validarValorNumericoLlamada(mesesDeVida);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese la estacion de plantacion la planta por favor(otonio, invierno, primavera, verano, anual)");
+									estacionPlantacion=scan.nextLine();
+								    mensaje=Planta.validarEstacionLlamada(estacionPlantacion);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese el habitat de la planta por favor(rio, lago, arrecife, estanque, pantano, delta)");
+									habitat=scan.nextLine();
+								    mensaje=Planta.validarHabitatLlamada("acuatica", habitat);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese la altura actual de la planta por favor");
+									altura=scan.nextInt();
+									scan.nextLine();
+								    mensaje=Producto.validarValorNumericoLlamada(altura);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese si la planta posee flor por favor (true/false)");
+									tieneflor=scan.nextBoolean();
+								    mensaje=Producto.validarBooleanLlamada(tieneflor);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese el nivel de exposicion solar de la planta por favor (sombra, mediasombra, pleno sol)");
+									nivelDeExposicionSolar=scan.nextLine();
+								    mensaje=Planta.validarExpSolarLlamada(nivelDeExposicionSolar);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese el tipo de raiz de la planta por favor (axonomorfa, ramificada, fascicular, tuberosa, napiforme, adventicia)");
+									tipoRaiz=scan.nextLine();
+								    mensaje=Planta.validarTipoRaizLlamada(tipoRaiz);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese si la planta es aromatica por favor (true/false)");
+									esAromatica=scan.nextBoolean();
+								    mensaje=Producto.validarBooleanLlamada(esAromatica);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese el tipo de agua de la planta por favor (dulce, salada)");
+									tipoDeAgua=scan.nextLine();
+								    mensaje=PlantaAcuatica.validarTipoDeAguaLlamada(tipoDeAgua);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								mensaje="";
+								while(mensaje!=null)
+								{
+									System.out.println("Ingrese la temperatura promedio del agua de la planta por favor (mayor o igual a 20 y menor o igual a 27)");
+									temperaturaAgua=scan.nextInt();
+									scan.nextLine();
+								    mensaje=PlantaAcuatica.validarTempAguaLlamada(temperaturaAgua);
+								    if(mensaje!=null)
+								    {
+								    	System.out.println(mensaje);
+								    }
+								} 
+								
+								
+								
+					
+							
+							/*
+							 * String nombre, String marca, int stock, String descripcion, int mesesDeVida,
+			String estacionPlantacion, String habitat, int altura, boolean flor,
+			String nivelExposicionSolar, String tipoRaiz, boolean aromatica, String tipoDeAgua, int temperaturaAgua,
+			String durezaAgua, String tipo
+							 */
+							
+							
+							break;
+						}
+						default:
+							throw new IllegalArgumentException("Unexpected value: " + key);
+						}
 				     break;
 					}
 					default:
